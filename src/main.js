7799980df064c42426b4e3ea939651d042550b3e
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-13 12:12:03 +0200

    add img add fontawsome

--- src/main.js
+++ src/main.js
@@ -6,6 +6,35 @@ import 'bootstrap/scss/bootstrap.scss'
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
 
+
+
+/* Font Awesome icons*/
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
+import { faClock as fasClock } from '@fortawesome/free-solid-svg-icons'
+import { faPhone as fasPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope as fasEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUser as farUser } from '@fortawesome/free-regular-svg-icons'
+import { faTruckRampBox as fasTruckRampBox } from '@fortawesome/free-solid-svg-icons'
+import { faTemperatureLow as fasTemperatureLow } from '@fortawesome/free-solid-svg-icons'
+import { faBoxesStacked as fasBoxesStacked } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot as fasLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faQuoteRight as fasQuoteRight } from '@fortawesome/free-solid-svg-icons'
+
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
+library.add(fasClock, fasPhone, fasEnvelope, farUser, fasTruckRampBox, fasTemperatureLow, fasBoxesStacked, fasLocationDot, fasQuoteRight) 
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
