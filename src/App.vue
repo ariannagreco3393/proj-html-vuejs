11c3e9519c4a6553aaf2adad6760df62cfc04190
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-13 13:14:03 +0200

    add data structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    
+    <header id="site_header"></header>
+    <!-- /#site_header -->
+    <main id="site_main"></main>
+    <!-- /#site_main -->
+    <footer id="site_footer"></footer>
+    <!-- /#site_footer -->
   </div>
 </template>
 
@@ -8,15 +13,150 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      mainMenu: [
+        {
+          id: 1,
+          href: "#",
+          text: "HOME",
+        },
+        {
+          id: 2,
+          href: "#",
+          text: "ABOUT",
+        },
+        {
+          id: 3,
+          href: "#",
+          text: "SERVICES",
+        },
+        {
+          id: 4,
+          href: "#",
+          text: "PROCESS",
+        },
+        {
+          id: 5,
+          href: "#",
+          text: "TESTIMONIALS",
+        },
+      ],
+      secondaryMenu: [
+        {
+          title: "About",
+          links: [
+            {
+              id: 1,
+              href: "#",
+              text: "The Company",
+            },
+            {
+              id: 2,
+              href: "#",
+              text: "Institutional",
+            },
+            {
+              id: 3,
+              href: "#",
+              text: "Social & Events",
+            },
+            {
+              id: 4,
+              href: "#",
+              text: "Innovation",
+            },
+            {
+              id: 5,
+              href: "#",
+              text: "Environment",
+            },
+            {
+              id: 6,
+              href: "#",
+              text: "Technology",
+            },
+          ],
+        },
+        {
+          title: "Transport",
+          links: [
+            {
+              id: 1,
+              href: "#",
+              text: "Industrialized",
+            },
+            {
+              id: 2,
+              href: "#",
+              text: "Chemicals",
+            },
+            {
+              id: 3,
+              href: "#",
+              text: "Packaged Liquids",
+            },
+            {
+              id: 4,
+              href: "#",
+              text: "Construction",
+            },
+            {
+              id: 5,
+              href: "#",
+              text: "Laminated Wood",
+            },
+            {
+              id: 6,
+              href: "#",
+              text: "And others",
+            },
+          ],
+        },
+        {
+          title: "Support",
+          links: [
+            {
+              id: 1,
+              href: "#",
+              text: "Responsibility",
+            },
+            {
+              id: 2,
+              href: "#",
+              text: "Terms of Use",
+            },
+            {
+              id: 3,
+              href: "#",
+              text: "About Cookies",
+            },
+            {
+              id: 4,
+              href: "#",
+              text: "Privacy Policy",
+            },
+            {
+              id: 5,
+              href: "#",
+              text: "Accessibility",
+            },
+            {
+              id: 6,
+              href: "#",
+              text: "Information",
+            },
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
 /* style*/
-@import '@/assets/scss/style';
+@import "@/assets/scss/style";
 /* colors palette */
-@import '@/assets/scss/variables.scss';
-
-
-
+@import "@/assets/scss/variables.scss";
 </style>
