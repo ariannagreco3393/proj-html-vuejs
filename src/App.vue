b80efed829267114b216d642d1b936d75a3ee395
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-16 20:33:53 +0200

    add footer top component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <div id="app">
-    <headerComponent/>
+    <headerComponent />
     <!-- /#site_header -->
-    <mainComponent/>
+    <mainComponent />
     <!-- /#site_main -->
-    <footer id="site_footer"></footer>
+    <footer id="site_footer">
+     <footerTopComponent/>
+    </footer>
     <!-- /#site_footer -->
   </div>
 </template>
 
 <script>
-import headerComponent from '@/components/headerComponent.vue'
-import mainComponent from '@/components/mainComponent.vue'
+import headerComponent from "@/components/headerComponent.vue";
+import mainComponent from "@/components/mainComponent.vue";
+import footerTopComponent from "@/components/footerTopComponent.vue";
 export default {
   name: "App",
   components: {
     headerComponent,
-    mainComponent
+    mainComponent,
+    footerTopComponent
   },
   data() {
     return {
-      
       secondaryMenu: [
         {
           title: "About",
@@ -137,5 +140,6 @@ export default {
 /* style*/
 @import "@/assets/scss/style";
 @import "@/assets/scss/partials/_commonRules";
+
 
 </style>
---
+++ src/components/footerTopComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="footer_top">
+    <div class="container">
+      <div class="row">
+        <div class="col-3 logo">
+          <a class="navbar-brand nex text_green" href="#">NEX</a>
+          <a class="navbar-brand gen text-light" href="#">GEN</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "footerTopComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+@import "@/assets/scss/partials/_commonRules";
+
+.container {
+  max-width: 1200px;
+}
+.footer_top {
+  background-color: $font-dark;
+  padding: 6rem 0;
+  .logo {
+    font-weight: 500;
+    .nex {
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;
+      padding-left: 2rem;
+      padding-right: 3px;
+      background-color: rgba(3, 132, 132, 0.257);
+    }
+    .gen {
+      margin-left: -0.7rem;
+    }
+  }
+}
+</style>
