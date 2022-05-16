411369c69d95c50c363e4225c349dee817dd4d37
ariannagreco3393 <a.greco3393@gmail.com>
2022-05-16 10:35:30 +0200

    add main services icon

--- src/components/mainComponent.vue
+++ src/components/mainComponent.vue
@@ -1,99 +1,18 @@
 <template>
   <main id="site_main">
     <whoWeAre />
-    <section class="main_services bg_dark">
-      <div class="container">
-        <div class="row">
-          <small class="text_green font_500">ROAD TRANSPORT</small>
-          <div class="excellence d-flex align-items-center mt-4">
-            <h2 class="text-light font_900">Main</h2>
-            <h2 class="services text-light font_900">Services</h2>
-          </div>
-          <div class="col-8">
-            <p class="text-light font_200">
-              With all of this expertise and capability comes an unrivalled
-              commitment tu customer service. We eill word hard to understand
-              your needs in order to develop a produvtive, long-term
-              partenrship.
-            </p>
-          </div>
-          <div class="col-4 text-end align-self-center">
-            <button class="transparent_btn">SEE ALL</button>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4">
-            <div class="square text-light">
-              <div class="d-flex justify-content-between">
-                <font-awesome-icon
-                  class="truck_icon"
-                  icon="fa-solid fa-truck-ramp-box"
-                />
-
-                <font-awesome-icon
-                  class="arrow_icon"
-                  icon="fa-solid fa-arrow-right"
-                />
-              </div>
-              <h5 class="mt-3">Technology</h5>
-              <p class="font_200 mt-3">
-                We are continualy focused on developing technology solutions
-                adapted our client's needs.
-              </p>
-            </div>
-          </div>
-          <div class="col-4">
-            <div class="square text-light">
-              <div class="d-flex justify-content-between">
-                <font-awesome-icon
-                  class="truck_icon"
-                  icon="fa-solid fa-truck-ramp-box"
-                />
-
-                <font-awesome-icon
-                  class="arrow_icon"
-                  icon="fa-solid fa-arrow-right"
-                />
-              </div>
-              <h5 class="mt-3">Technology</h5>
-              <p class="font_200 mt-3">
-                We are continualy focused on developing technology solutions
-                adapted our client's needs.
-              </p>
-            </div>
-          </div>
-          <div class="col-4">
-            <div class="square text-light">
-              <div class="d-flex justify-content-between">
-                <font-awesome-icon
-                  class="truck_icon"
-                  icon="fa-solid fa-truck-ramp-box"
-                />
-
-                <font-awesome-icon
-                  class="arrow_icon"
-                  icon="fa-solid fa-arrow-right"
-                />
-              </div>
-              <h5 class="mt-3">Technology</h5>
-              <p class="font_200 mt-3">
-                We are continualy focused on developing technology solutions
-                adapted our client's needs.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <mainServices />
   </main>
 </template>
 
 <script>
 import whoWeAre from "@/components/whoWeAreComponent.vue";
+import mainServices from "@/components/mainServicesComponent.vue";
 export default {
   name: "mainComponent",
   components: {
     whoWeAre,
+    mainServices,
   },
 };
 </script>
@@ -101,6 +20,4 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/scss/variables.scss";
 @import "@/assets/scss/partials/_commonRules";
-
-
 </style>
